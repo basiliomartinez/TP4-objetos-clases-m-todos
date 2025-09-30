0b0d6e29da7386bddcb8a77ab8d60142e94c2cb1
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-09-30 07:58:39 -0300

    Ejercicio 2 resuelto

--- js/Ejercicio2.js
+++ js/Ejercicio2.js
@@ -23,13 +23,13 @@ document.writeln para mostrar los resultados en el navegador.
 
 //Clase que modela una cuenta bancaria
 
-class cuenta {
+class Cuenta {
   #titular;
   #saldo;
 
   constructor(titular) {
-    this.titular = titular; //nombre del titular
-    this.saldo = 0; //saldo inicial
+    this.#titular = titular; //nombre del titular
+    this.#saldo = 0; //saldo inicial
   }
 
   //Getter del titular
@@ -40,4 +40,51 @@ class cuenta {
   get(saldo) {
     return this.#saldo;
   }
+
+  //Metodo para ingresar dinero
+  ingresar(cantidad) {
+    if (cantidad > 0) {
+      this.#saldo += cantidad; //sumo al saldo
+      document.writeln(`<p>Se ingresaron $ ${cantidad}</p>`);
+    } else {
+      document.writeln(`<p>La cantidad debe ser positiva</p>`);
+    }
+  }
+
+  //Metodo para extraer dinero
+  extraer(cantidad) {
+    if (cantidad > 0 && cantidad <= this.#saldo) {
+      this.#saldo -= cantidad; //resto al saldo
+      document.writeln(`<p>Se extrajeron $ ${cantidad}</p>`);
+    } else {
+      document.writeln(`No se puede extraer dinero`);
+    }
+  }
+
+  //Metodo para mostrar la informacion de la cuenta
+  informar() {
+    document.writeln(`<h3>Estado de la cuenta</h3>`);
+    document.writeln(`<ul>
+    <li>Titular : ${this.#titular}</li>
+    <li>Saldo : $${this.#saldo}</li>
+    </ul>`);
+  }
 }
+//Usamos la clase
+//Creamos un objeto "cuenta" con titular Alex
+const cuenta = new Cuenta("Alex");
+
+//Mostrar estado inicial
+console.log("Estado Inicial, cuenta");
+cuenta.informar();
+
+// Ingreso dinero
+cuenta.ingresar(500);
+console.log("Despues de ingresar dinero:", cuenta);
+
+// Extraccion de dinero
+cuenta.extraer(200);
+console.log("Desoues de extraer dinero:", cuenta);
+
+// Mostrar estado final
+cuenta.informar();
